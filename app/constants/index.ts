55cf0310efea4f1d5d3da8bfb030be83b8c01806
Aditya Singh Rathore <adityasingh.rathore0807@gmail.com>
2026-06-29 19:49:17 +0530

    corret index file

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -3,4 +3,13 @@
 //AI response Format - defines the structure strict format that AI should return
 //prepareInstructions - generate a prompt for AI to analyzer the resume ine xact format
 
- export const resume; { } [] = [{}]
+
+export const resumes: Resumes [] = [
+    { id: "1" /* other properties */ },
+    { id: "2" /* other properties */ },
+    { id: "3" /* other properties */ },
+];
+export const AIResponseformat = `
+Your string here.
+You can write multiple lines.
+`;
